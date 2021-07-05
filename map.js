24333e2ca01bb5a78e24aa22bb7ebc03fff2fe3a
Attila Toth <att.toth1991@gmail.com>
2021-07-05 20:46:34 +0200

    Sas és a téglák körülötte  dinamikusan középre kerülnek pályamódosítás során

--- map.js
+++ map.js
@@ -1,32 +1,26 @@
-const table = require('table');
+const table = require('table').table;
 
 const generateMap = (width, height) => {
-    const arr = new Array(height);
-    for (let i = 0; i < arr.length; i++){
-        arr[i] = new Array(width);
+  const arr = new Array(height);
+  const PositionEagle = (arr.length - 1) / 2;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(width);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[0][j] = 'F';
+      arr[arr.length - 1][j] = 'F';
+      arr[i][0] = 'F';
+      arr[i][arr.length - 1] = 'F';
+      arr[arr.length - 2][PositionEagle] = 'E';
+      arr[arr.length - 2][PositionEagle - 1] = 'B';
+      arr[arr.length - 2][PositionEagle + 1] = 'B';
+      arr[arr.length - 3][PositionEagle - 1] = 'B';
+      arr[arr.length - 3][PositionEagle + 1] = 'B';
+      arr[arr.length - 3][PositionEagle] = 'B';
     }
-    return arr;
+  }
+  return arr;
 };
 
-const fillMap = (arr) => {
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 0; j < arr[i].length; j++){
-            arr[i][j] = '';
-            arr[0][j] = 'F';
-            arr[i][0] = 'F';
-            arr[arr.length - 1][j] = 'F';
-            arr[i][arr[i].length - 1] = 'F';
-        }
-    }
-    arr[23][12] = 'E';
-    arr[23][11] = 'B';
-    arr[23][13] = 'B';
-    arr[22][11] = 'B';
-    arr[22][12] = 'B';
-    arr[22][13] = 'B';
-
-
-    return arr;
-};
-
-console.log(table.table(fillMap(generateMap(25, 25))));
+console.log(table(generateMap(25, 25)));

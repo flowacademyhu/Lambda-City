79fb81cdfef404fa17528ec3af4cf24daec9f2c9
Attila Toth <att.toth1991@gmail.com>
2021-07-08 09:32:12 +0200

    map és input js

---
+++ PlayerInput.js
@@ -0,0 +1,111 @@
+const table = require('table').table;
+const emptyField = '';
+
+const generateMap = (width, height) => {
+  const arr = new Array(height);
+  const PositionEagle = (arr.length - 1) / 2;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(width);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = emptyField;
+      arr[0][j] = 'F';
+      arr[arr.length - 1][j] = 'F';
+      arr[i][0] = 'F';
+      arr[i][arr.length - 1] = 'F';
+      arr[arr.length - 2][PositionEagle] = 'E';
+      arr[arr.length - 2][PositionEagle - 1] = 'B';
+      arr[arr.length - 2][PositionEagle + 1] = 'B';
+      arr[arr.length - 3][PositionEagle - 1] = 'B';
+      arr[arr.length - 3][PositionEagle + 1] = 'B';
+      arr[arr.length - 3][PositionEagle] = 'B';
+    }
+  }
+  return arr;
+};
+
+const map = generateMap(11, 11);
+
+const player = {
+  tank: 'P',
+  life: 3,
+  spawnPointX: map.length - 2,
+  spawnPointY: (map.length - 1) / 2 - 2,
+  posX: 0,
+  posY: 0
+};
+
+const printMap = (map) => {
+  console.log(table(map));
+};
+
+const playerInput = (arr) => {
+  if (
+    player.spawnPointX === arr.length - 2 &&
+    player.spawnPointY === (arr.length - 1) / 2 - 2
+  ) {
+    arr[arr.length - 2][(arr.length - 1) / 2 - 2] = player.tank;
+    player.posX = player.spawnPointX;
+    player.posY = player.spawnPointY;
+    printMap(arr);
+  }
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.resume();
+  stdin.setEncoding('utf8');
+  stdin.on('data', (key) => {
+    if (key === 'w') {
+      moveUp(arr);
+      arr[player.posX][player.posY] = player.tank;
+      printMap(arr);
+    } else if (key === 's') {
+      moveDown(arr);
+      arr[player.posX][player.posY] = player.tank;
+      printMap(arr);
+    } else if (key === 'a') {
+      moveLeft(arr);
+      arr[player.posX][player.posY] = player.tank;
+      printMap(arr);
+    } else if (key === 'd') {
+      moveRight(arr);
+      arr[player.posX][player.posY] = player.tank;
+      printMap(arr);
+    } else if (key === 'q') {
+      process.exit();
+    }
+  });
+};
+playerInput(map);
+
+const moveUp = (arr) => {
+  if (arr[player.posX - 1][player.posY] === emptyField) {
+    player.posX--;
+    arr[player.posX + 1][player.posY] = emptyField;
+  }
+  return arr;
+};
+
+const moveDown = (arr) => {
+  if (arr[player.posX + 1][player.posY] === emptyField) {
+    player.posX++;
+    arr[player.posX - 1][player.posY] = emptyField;
+  }
+  return arr;
+};
+
+const moveLeft = (arr) => {
+  if (arr[player.posX][player.posY - 1] === emptyField) {
+    player.posY--;
+    arr[player.posX][player.posY + 1] = emptyField;
+  }
+  return arr;
+};
+
+const moveRight = (arr) => {
+  if (arr[player.posX][player.posY + 1] === emptyField) {
+    player.posY++;
+    arr[player.posX][player.posY - 1] = emptyField;
+  }
+  return arr;
+};

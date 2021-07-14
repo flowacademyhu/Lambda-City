886281470825e32791fbfb152dd9fbad957ebb67
Sisák Ádám <sisakadam@gmail.com>
2021-07-14 20:51:13 +0200

    ...

--- highscore.js
+++ highscore.js
@@ -1,20 +1,9 @@
-const readLine = require('readline-sync'); // Nem fogja megtalálni, telepítsük NPM-el!
+const readLine = require('readline-sync');
 const fs = require('fs');
-const word = readLine.question('Hogy hívnak bajnok?'); // Ez kiírja konzolra a megadott
-const points = 69;
-// // szöveget, és egy enter leütéséig beolvassa az adatokat, majd letárolja
-// // változóban.
-// const point = readLine.question('Mennyi pontod lett?');
+const word = readLine.question('Hogy hívnak bajnok?'); // GAME OVER ESETÉN ELŐHÍVANDÓ!!!!
+const points = 69; // PONTOK TÁROLÁSA, VÁLTOZÓBA ADÁS GAME OVER ESETÉN!!!!!!
 
 // fájlbaírás függvénye!
-
-// fs.readFile("users.txt", (err, file) => {
-//   if(file.length == 0){
-//       console.log("file is empty")
-//   }else{
-//       console.log("file is not empty")
-//   }
-// })
 
 const highscore = (name, points) => {
   fs.appendFileSync('message.txt', `\n${name}  :  ${points} pont `);
@@ -23,14 +12,14 @@ const highscore = (name, points) => {
 // játékos név beírása scoreboardra!
 highscore(word, points);
 
-// scoreboard kiírása!
+// scoreboard utoljára beírt 5 elem tömbbe írása!!!
 
 const array = fs.readFileSync('message.txt').toString().split('\n');
 const tomb = [];
 for (let i = array.length - 1; i >= 0; i--) {
   tomb.push(array[i]);
 }
-
+// SCOREBOARD KIÍRÁS
 console.log(tomb.slice(0, 5));
 
 module.exports = {

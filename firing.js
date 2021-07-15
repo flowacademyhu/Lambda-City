5ead6dcfae46f2f8b8401ff7b3383a431a2377e7
Kovari Kristof <kovarik8505@gmail.com>
2021-07-15 13:36:37 +0200

    dead

--- firing.js
+++ firing.js
@@ -1,23 +1,42 @@
+const enemies = require('./enemy').enemies;
 const bulletout = {
   bullet: '*',
   posX: 0,
   posY: 0
 };
-
-const fireBullet = (arr, player, printMap, emptyField) => {
+const found = enemies.find(
+  (enemy) => enemy.posX === bulletout.posX && enemy.posY === bulletout.posY
+);
+const fireBullet = (arr, player, printMap, emptyField, enemy) => {
   bulletout.posX = player.posX;
   bulletout.posY = player.posY;
-
+  console.log(found);
   if (player.tank === '^') {
     let i = bulletout.posX - 1;
     const interval = setInterval(() => {
       if (arr[i + 1][bulletout.posY] !== player.tank) {
         arr[i + 1][bulletout.posY] = emptyField;
       }
+      if (
+        arr[i][bulletout.posY] === '^' ||
+        arr[i][bulletout.posY] === 'v' ||
+        arr[i][bulletout.posY] === '<' ||
+        arr[i][bulletout.posY] === '>'
+      ) {
+        // const found = enemies.find(
+        //   (enemy) => enemy.posX === i && enemy.posY === bulletout.posY
+        // );
+        if (found) {
+          found.status = 'dead';
+          arr[i][bulletout.posY] = emptyField;
+          console.log(found);
+        }
+      }
       if (arr[i][bulletout.posY] !== emptyField) {
         if (arr[i][bulletout.posY] !== 'F') {
           arr[i][bulletout.posY] = emptyField;
         }
+
         clearInterval(interval);
       } else {
         arr[i][bulletout.posY] = bulletout.bullet;
@@ -32,6 +51,17 @@ const fireBullet = (arr, player, printMap, emptyField) => {
     const interval = setInterval(() => {
       if (arr[i - 1][bulletout.posY] !== player.tank) {
         arr[i - 1][bulletout.posY] = emptyField;
+      }
+      if (
+        arr[i][bulletout.posY] === '^' ||
+        arr[i][bulletout.posY] === 'v' ||
+        arr[i][bulletout.posY] === '<' ||
+        arr[i][bulletout.posY] === '>'
+      ) {
+        if (found) {
+          found.status = 'dead';
+          arr[i][bulletout.posY] = emptyField;
+        }
       }
       if (arr[i][bulletout.posY] !== emptyField) {
         if (arr[i][bulletout.posY] !== 'F') {
@@ -52,6 +82,20 @@ const fireBullet = (arr, player, printMap, emptyField) => {
       if (arr[bulletout.posX][i + 1] !== player.tank) {
         arr[bulletout.posX][i + 1] = emptyField;
       }
+      if (
+        arr[i][bulletout.posY] === '^' ||
+        arr[i][bulletout.posY] === 'v' ||
+        arr[i][bulletout.posY] === '<' ||
+        arr[i][bulletout.posY] === '>'
+      ) {
+        // const found = enemies.find(
+        //   (enemy) => enemy.posX === bulletout.posX && enemy.posY === i
+        // );
+        if (found) {
+          found.status = 'dead';
+          arr[bulletout.posX][i] = emptyField;
+        }
+      }
       if (arr[bulletout.posX][i] !== emptyField) {
         if (arr[bulletout.posX][i] !== 'F') {
           arr[bulletout.posX][i] = emptyField;
@@ -71,6 +115,20 @@ const fireBullet = (arr, player, printMap, emptyField) => {
       if (arr[bulletout.posX][i - 1] !== player.tank) {
         arr[bulletout.posX][i - 1] = emptyField;
       }
+      if (
+        arr[i][bulletout.posY] === '^' ||
+        arr[i][bulletout.posY] === 'v' ||
+        arr[i][bulletout.posY] === '<' ||
+        arr[i][bulletout.posY] === '>'
+      ) {
+        // const found = enemies.find(
+        //   (enemy) => enemy.posX === bulletout.posX && enemy.posY === i
+        // );
+        if (found) {
+          found.status = 'dead';
+          arr[bulletout.posX][i] = emptyField;
+        }
+      }
       if (arr[bulletout.posX][i] !== emptyField) {
         if (arr[bulletout.posX][i] !== 'F') {
           arr[bulletout.posX][i] = emptyField;
@@ -85,5 +143,6 @@ const fireBullet = (arr, player, printMap, emptyField) => {
   }
 };
 module.exports = {
-  fireBullet
+  fireBullet,
+  found
 };
